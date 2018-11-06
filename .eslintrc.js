521a3edc26d56b25e06e710a2c700b11b7b2e9d8
Michael Kopinsky <Michael.Kopinsky@pennmedicine.upenn.edu>
2018-11-06 10:27:45 -0500

    A bit more eslint stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,12 @@ module.exports = {
 
       "func-names": ["warn", "never"],
       "prefer-arrow-callback": "off",
+      "no-unused-vars": ["warn", {"args": "none"}],
+      "max-len": "off"
+    },
+    "env": {
+        "browser": false,
+        "node": true
     }
+
 };
